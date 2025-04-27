996f38fa4efac9ff726924bb9595b5e5154782e1
juehm <juehm2017@163.com>
2025-04-28 02:28:03 +0800

    Use react-native-bootsplash to set the show a splash screen during app startup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
-import {ThemeProvider} from './src/provider/theme/ThemeContext';
-import {MainNavigator} from './src/router/MainNavigator';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { ThemeProvider } from './src/provider/theme/ThemeContext';
+import { MainNavigator } from './src/router/MainNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BootSplash from 'react-native-bootsplash';
+import { useEffect } from 'react';
 export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+    });
+  }, []);
   return (
     <ThemeProvider>
       <SafeAreaProvider>
